9038be7bef19ed6325b41f38870f8256dea7ea2b
SRP02 <pratamazone1@gmail.com>
2024-03-04 20:07:27 +0700

    ntahlah ,cm nambah onclick

--- app/src/main/java/com/example/belajarandroid10pplg1/AdapterSiswa10PPLG1.java
+++ app/src/main/java/com/example/belajarandroid10pplg1/AdapterSiswa10PPLG1.java
@@ -13,60 +13,68 @@ import com.bumptech.glide.Glide;
 
 import java.util.List;
 
-public class AdapterSiswa10PPLG1 extends  RecyclerView.Adapter<AdapterSiswa10PPLG1.ViewHolder> {
+public class AdapterSiswa10PPLG1 extends RecyclerView.Adapter<AdapterSiswa10PPLG1.ViewHolder> {
 
     private List<Item_Pelajar> students;
     private Context context;
 
+    // Constructor
     public AdapterSiswa10PPLG1(Context context, List<Item_Pelajar> students) {
         this.context = context;
         this.students = students;
     }
 
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    // ViewHolder class
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView textViewName;
         TextView textAbsen;
         ImageView imageView;
         TextView textAlamat;
+        ItemClickListener mClickListener; // Declare ItemClickListener
 
-        public ViewHolder(@NonNull View itemView) {
+        // Constructor
+        ViewHolder(View itemView) {
             super(itemView);
             textViewName = itemView.findViewById(R.id.tvNamaLengkap);
             textAbsen = itemView.findViewById(R.id.tvNoAbsen);
             imageView = itemView.findViewById(R.id.IVitem);
             textAlamat = itemView.findViewById(R.id.tvAlamat);
+            itemView.setOnClickListener(this); // Set click listener
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null)
+                mClickListener.onItemClick(view, getAdapterPosition());
         }
     }
 
-
+    // onCreateViewHolder method
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         View view = LayoutInflater.from(context).inflate(R.layout.item_view, parent, false);
         return new ViewHolder(view);
     }
 
-
+    // onBindViewHolder method
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
         Item_Pelajar student = students.get(position);
         holder.textViewName.setText(student.getName());
         holder.textAbsen.setText(student.getAbsen());
         holder.textAlamat.setText(student.getAlamat());
-
-// Memuat gambar menggunakan Glide
-        Glide.with(context)
-                .load(student.getImageResource())
-                .into(holder.imageView);
+        Glide.with(context).load(student.getImageResource()).into(holder.imageView);
     }
 
-
+    // getItemCount method
     @Override
     public int getItemCount() {
         return students.size();
     }
-}
 
+    // Interface for click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/example/belajarandroid10pplg1/ListSiswa10PPLG1.java
+++ app/src/main/java/com/example/belajarandroid10pplg1/ListSiswa10PPLG1.java
@@ -5,11 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListSiswa10PPLG1 extends AppCompatActivity {
+public class ListSiswa10PPLG1 extends AppCompatActivity implements AdapterSiswa10PPLG1.ItemClickListener {
 
     private RecyclerView recyclerView;
     private AdapterSiswa10PPLG1 adapter;
@@ -28,32 +30,89 @@ public class ListSiswa10PPLG1 extends AppCompatActivity {
         siswa1.setName("Royyan");
         siswa1.setAlamat("Surabaya");
         siswa1.setAbsen("33");
-        siswa1.setImageResource("https://www.thesportsdb.com//images//media//team//badge//xzqdr11517660252.png");
+        siswa1.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg");
         pelajar.add(siswa1);
 
         Item_Pelajar siswa2 = new Item_Pelajar();
         siswa2.setName("Ocean");
         siswa2.setAlamat("Jogja");
         siswa2.setAbsen("29");
-        siswa2.setImageResource("https://www.thesportsdb.com//images//media//team//badge//xzqdr11517660252.png");
+        siswa2.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg");
         pelajar.add(siswa2);
 
         Item_Pelajar siswa3 = new Item_Pelajar();
         siswa3.setName("Burhan");
         siswa3.setAlamat("Purbalingga");
         siswa3.setAbsen("9");
-        siswa3.setImageResource("https://www.thesportsdb.com//images//media//team//badge//xzqdr11517660252.png");
+        siswa3.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg");
         pelajar.add(siswa3);
 
         Item_Pelajar siswa4 = new Item_Pelajar();
         siswa4.setName("Satria");
         siswa4.setAlamat("Pati");
         siswa4.setAbsen("35");
-        siswa4.setImageResource("https://www.thesportsdb.com//images//media//team//badge//xzqdr11517660252.png"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        siswa4.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
         pelajar.add(siswa4);
+
+        Item_Pelajar siswa5 = new Item_Pelajar();
+        siswa5.setName("Dzaky");
+        siswa5.setAlamat("Bekasi");
+        siswa5.setAbsen("12");
+        siswa5.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        pelajar.add(siswa5);
+
+        Item_Pelajar siswa6 = new Item_Pelajar();
+        siswa6.setName("Nico");
+        siswa6.setAlamat("Jakarta");
+        siswa6.setAbsen("28");
+        siswa6.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        pelajar.add(siswa6);
+
+        Item_Pelajar siswa7 = new Item_Pelajar();
+        siswa7.setName("Afdhal");
+        siswa7.setAlamat("Tangerang");
+        siswa7.setAbsen("27");
+        siswa7.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        pelajar.add(siswa7);
+
+        Item_Pelajar siswa8 = new Item_Pelajar();
+        siswa8.setName("Hatta");
+        siswa8.setAlamat("Jakarta");
+        siswa8.setAbsen("26");
+        siswa8.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        pelajar.add(siswa8);
+
+        Item_Pelajar siswa9 = new Item_Pelajar();
+        siswa9.setName("Farras");
+        siswa9.setAlamat("Kudus");
+        siswa9.setAbsen("18");
+        siswa9.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        pelajar.add(siswa9);
+
+        Item_Pelajar siswa10 = new Item_Pelajar();
+        siswa10.setName("Nail");
+        siswa10.setAlamat("Jakarta");
+        siswa10.setAbsen("25");
+        siswa10.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        pelajar.add(siswa10);
+
+        Item_Pelajar siswa11 = new Item_Pelajar();
+        siswa11.setName("Rizal");
+        siswa11.setAlamat("Sumbawa");
+        siswa11.setAbsen("23");
+        siswa11.setImageResource("https://i.pinimg.com/736x/4e/22/be/4e22beef6d94640c45a1b15f4a158b23.jpg"); // Menggunakan metode setImageResource() untuk menetapkan URL gambar
+        pelajar.add(siswa11);
+
 
         adapter = new AdapterSiswa10PPLG1(this, pelajar);
         recyclerView.setAdapter(adapter);
 
+    }
+    @Override
+    public void onItemClick(View view, int position) {
+        String nama = pelajar.get(position).getName();
+        String absen = pelajar.get(position).getAbsen();
+        String alamat = pelajar.get(position).getAlamat();
+        Toast.makeText(this, "Nama : " + nama + "\nNo absen : " + absen + "\nAlamat :" +alamat, Toast.LENGTH_SHORT).show();
     }
 }
